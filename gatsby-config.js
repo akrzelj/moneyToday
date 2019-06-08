7314c2a22beaa3c09597da09c958a88d1f74bdaa
etnaetna <akrzel00@fesb.hr>
2019-06-08 14:47:08 +0200

    content reading added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,28 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: "posts",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1000,
+              linkImagesToOriginal: false,
+              backgroundColor: "transparent",
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,79 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
+import MainPost from "../components/MainPost/index"
+import SecondPost from "../components/SecondPost/index"
+import OtherPost from "../components/OtherPost/index"
 
-export default () => (
-  <Layout>
-    <div />
-    <div />
-    <div />
-  </Layout>
-)
+export default ({ data }) => {
+  if (data.allMarkdownRemark === null) return <div>NO DATA</div>
+
+  let posts = data.allMarkdownRemark.edges
+  let noviPostovi = []
+
+  posts.forEach(post => {
+    const title = post.node.frontmatter.title
+    const type = post.node.frontmatter.type
+    const thumbnail = post.node.frontmatter.indexImage.childImageSharp.fluid
+    const content = post.node.excerpt
+    if (posts.indexOf(post) === 0) {
+      noviPostovi.push(
+        <MainPost
+          title={title}
+          fluid={thumbnail}
+          to={type + "/" + post.node.frontmatter.slug}
+        >
+          {content}
+        </MainPost>
+      )
+    } else if (posts.indexOf(post) === 1 || posts.indexOf(post) === 2) {
+      noviPostovi.push(
+        <SecondPost
+          title={title}
+          fluid={thumbnail}
+          to={type + "/" + post.node.frontmatter.slug}
+        >
+          {content}
+        </SecondPost>
+      )
+    } else {
+      noviPostovi.push(
+        <OtherPost
+          title={title}
+          fluid={thumbnail}
+          to={type + "/" + post.node.frontmatter.slug}
+        >
+          {content}
+        </OtherPost>
+      )
+    }
+  })
+
+  return <Layout> {noviPostovi} </Layout>
+}
+
+export const query = graphql`
+  {
+    allMarkdownRemark(filter: { frontmatter: { type: { eq: "news" } } }) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            type
+            indexImage {
+              childImageSharp {
+                fluid {
+                  aspectRatio
+                  sizes
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
