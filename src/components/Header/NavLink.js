b4dc3b7d2283a9bf3dcccea46cb8c4d3887436c1
etnaetna <akrzel00@fesb.hr>
2019-06-30 17:27:04 +0200

    imp for last commit

--- src/components/Header/NavLink.js
+++ src/components/Header/NavLink.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import styles from "./styles.module.css"
 
 export default ({ children, to }) => (
-  <Link to={to} className={styles.navBarEl}>
+  <Link to={to} className={styles.navBarEl} activeClassName={styles.aktivan}>
     <div className={styles.navBarElContent}>{children}</div>
   </Link>
 )
