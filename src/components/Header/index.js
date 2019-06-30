8146fdb4acfb4d5187a0cc90ef63ca27fa145d24
etnaetna <akrzel00@fesb.hr>
2019-06-30 15:21:57 +0200

    removed unused code from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,10 @@
 import React from "react"
+
 import NavLink from "./NavLink"
-
 import styles from "./styles.module.css"
-
-import { getUser, isLoggedIn } from "../../services/auth"
 import User from "../User"
 
 export default () => {
-  const content = { message: "", login: true }
-  if (isLoggedIn()) {
-    content.message = `Hello, ${getUser().name}`
-  } else {
-    content.message = "You are not logged in"
-  }
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.nav}>
