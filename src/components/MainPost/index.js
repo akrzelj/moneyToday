c6da65fe065c081665bd9f22fcb5633b89f4c058
etnaetna <akrzel00@fesb.hr>
2019-08-20 18:14:41 +0200

    Removed unused imports

--- src/components/MainPost/index.js
+++ src/components/MainPost/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import style from "./styles.module.css"
 import Img from "gatsby-image"
-import { Link } from "gatsby"
 
 export default ({ title, fluid, to, children }) => (
   <div className={style.post}>
